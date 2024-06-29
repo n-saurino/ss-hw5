f41945223345279a5fd7259dbf499d6d04cb200e
Nigel Saurino <nigelsaurino@gmail.com>
2024-06-29 17:15:50 -0400

    Starting ex3

---
+++ ex3.cpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <mutex>
+#include <thread>
+
+/*
+- Implement a producer-consumer scenario using threads, mutexes, and condition variables.
+- One thread (the producer) should generate numbers and place them in a shared queue.
+- Another thread (the consumer) should read numbers from the queue and print them.
+- Use std::condition_variable to synchronize the producer and consumer.
+*/
+
+
+
+void Ex3(){
+
+}
