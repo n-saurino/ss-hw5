daf5031e63d28048d14ca60a14d5a4e18de10a54
Nigel Saurino <nigelsaurino@gmail.com>
2024-07-01 15:25:04 -0400

    Messing around with condition_variables

--- ex3.cpp
+++ ex3.cpp
@@ -1,6 +1,10 @@
 #pragma once
+#include <chrono>
+#include <condition_variable>
+#include <deque>
 #include <mutex>
 #include <thread>
+#include "ScopedThread.cpp"
 
 /*
 - Implement a producer-consumer scenario using threads, mutexes, and condition variables.
@@ -9,8 +13,55 @@
 - Use std::condition_variable to synchronize the producer and consumer.
 */
 
+std::deque<int> shared_q;
+std::condition_variable c;
+std::mutex ex3_mutex;
+
+void ThreadSafeEnque(int enque_val){
+    // acquire the mutex
+    std::lock_guard<std::mutex> lgm(ex3_mutex);
+    shared_q.push_back(enque_val);
+    c.notify_one();
+}
+
+void Produce(int val){
+    // Loop to a certain value
+
+    for(int i = 0; i < val; ++i){
+        ThreadSafeEnque(i);
+        // Bake in a delay
+        std::this_thread::sleep_for(std::chrono::microseconds(rand()% 100 + 1));
+    }
+}
+
+void ThreadSafeDeque(){
+    // acquire the mutex
+    std::unique_lock<std::mutex> lgm(ex3_mutex);
+    c.wait(lgm);
+    int val = shared_q.front();
+    shared_q.pop_front();    
+    std::cout << val << ", ";
+}
+
+void Consume(int val){
+    for(int i = 0; i < val; ++i){
+    ThreadSafeDeque();        
+    // std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 100 + 1));
+    }
+
+}
 
 
 void Ex3(){
 
+    int vals = 101;
+    
+    // create a producer thread
+    std::thread producer(Produce, vals);
+    ScopedThread scoped_producer(std::move(producer));
+    
+    // create a consumer thread
+    std::thread consumer(Consume, vals);
+    ScopedThread scoped_consumer(std::move(consumer));
+   
 }
--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include "ex1.cpp"
 #include "ex2.cpp"
+#include "ex3.cpp"
 
 int main(int, char**){
-    Ex2();
+    Ex3();
 }
