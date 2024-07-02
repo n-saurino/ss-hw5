cd04da8809cf1ba2618fcfd9ba5615a544658121
Nigel Saurino <nigelsaurino@gmail.com>
2024-07-02 16:13:38 -0400

    Need to finish the CancelOrder() function. Probably don't need ModifyOrder for this exercise since it's more focused on the multithreading piece. Then going to make Orderbook thread-safe

--- ex5.cpp
+++ ex5.cpp
@@ -44,11 +44,49 @@ typedef int64_t Price;
 typedef int32_t Quantity;
 typedef int32_t OrderId;
 
-struct Order{
+class Order{
+private:
     OrderId order_id_;
     Price price_;
-    Quantity quantity_;
+    Quantity initial_quantity_;
+    Quantity remaining_quantity_;
     Side side_;
+
+public:
+    Order(OrderId order_id, Price price, Quantity quantity, Side side): order_id_(order_id), price_(price), 
+                                                                        initial_quantity_(quantity), remaining_quantity_(quantity),
+                                                                        side_(side){
+                                                    
+    }
+
+    ~Order(){
+
+    }
+
+    OrderId GetOrderId(){
+        return order_id_;
+    }
+
+    void SetPrice(Price new_price){
+        price_ = new_price;
+    }
+
+    Price GetPrice() const{
+        return price_;
+    }
+
+    void SetRemainingQuantity(Quantity new_quantity){
+        remaining_quantity_ = new_quantity;
+    }
+
+    Quantity GetRemainingQuantity() const{
+        return remaining_quantity_;
+    }
+
+    Side GetSide() const{
+        return side_;
+    }
+
 };
 
 typedef Order* POrder;
@@ -68,6 +106,35 @@ public:
     }
 
     ~Orderbook(){
+
+    }
+
+    void AddOrder(const POrder& new_order){
+        orders_[new_order->GetOrderId()] = new_order;
+        if(new_order->GetSide() == Side::Bid){
+            bids_[new_order->GetPrice()].push_back(new_order);
+        }else{
+            asks_[new_order->GetPrice()].push_back(new_order);
+        }
+    }
+
+    void CancelOrder(const POrder& cancel_order){
+        if(orders_.find(cancel_order->GetOrderId()) == orders_.end()){
+            throw std::logic_error("Order ID not found in Orders");
+        }
+
+        orders_.erase(cancel_order->GetOrderId());
+        if(cancel_order->GetSide() == Side::Bid){
+            PriceLevel& order_price_level = bids_[cancel_order->GetPrice()];
+            auto it = std::find(order_price_level.begin(), order_price_level.end(), cancel_order);
+            bids_[cancel_order->GetPrice()].erase(it);
+            if(bids_[cancel_order->GetPrice()].empty()){
+                bids_.erase(cancel_order->GetPrice());
+            }
+        }
+    }
+
+    void ModifyOrder(){
 
     }
 };
