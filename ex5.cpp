a4ff766cab6bb64a21d0f86162fad784b0b03b40
Nigel Saurino <nigelsaurino@gmail.com>
2024-07-02 15:10:42 -0400

    Laid framework for Ex5. Need to implement orderbook function, getters and setters for Orders, and make the orderbook thread-safe

--- ex5.cpp
+++ ex5.cpp
@@ -14,7 +14,8 @@
 */
 
 /*
-    
+    BLUEPRINT
+
     struct Order:
         int64_t Price
         int32_t Quantity
@@ -25,7 +26,53 @@
     vector<POrder> PriceLevel
 
     Class Orderbook:
-    map<Price, vector<POrder>> orders_at_price_
-    map<OrderId, POrder Orders> orders_
+        map<Price, vector<POrder>> orders_at_price_
+        map<OrderId, POrder Orders> orders_
+
+        AddOrder()
+        CancelOrder()
+        ModifyOrder()
 
 */
+
+enum class Side{
+    Bid,
+    Ask
+};
+
+typedef int64_t Price;
+typedef int32_t Quantity;
+typedef int32_t OrderId;
+
+struct Order{
+    OrderId order_id_;
+    Price price_;
+    Quantity quantity_;
+    Side side_;
+};
+
+typedef Order* POrder;
+typedef std::vector<POrder> PriceLevel;
+typedef std::map<Price, PriceLevel> OrdersAtPrice;
+typedef std::map<OrderId, POrder> Orders;
+
+class Orderbook{
+private:
+    OrdersAtPrice bids_;
+    OrdersAtPrice asks_;
+    Orders orders_;
+
+public:
+    Orderbook(){
+
+    }
+
+    ~Orderbook(){
+
+    }
+};
+
+int Ex5(){
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#pragma once
-#include <stdio.h>
 #include "ex1.cpp"
 #include "ex2.cpp"
 #include "ex3.cpp"
